14038a2bc13d71a4c0304deb0c2977f42eac981c
xiaoling <xp2185@columbia.edu>
2021-04-13 11:42:10 +0000

    update views!

--- adopt/views.py
+++ adopt/views.py
@@ -61,66 +61,19 @@ def add_sights(request):
 
     return render(request, 'Sightings/add.html', context)
 
-
-def stats_view(request):
-
-    sights = Sighting.objects.all()
-
-    # shift
-    AM_n = sights.filter(Shift='AM').count()
-    PM_n = sights.filter(Shift='PM').count()
-    AM_pct = AM_n/(AM_n + PM_n)
-    AM_pct = "{:.2%}".format(AM_pct)
-    PM_pct = PM_n/(AM_n + PM_n)
-    PM_pct = "{:.2%}".format(PM_pct)
-    # age
-    Juvenile_n = sights.filter(Age='Juvenile').count()
-    Adult_n = sights.filter(Age='Adult').count()
-    Juvenile_pct = Juvenile_n / (Juvenile_n + Adult_n)
-    Juvenile_pct = "{:.2%}".format(Juvenile_pct)
-    Adult_pct = Adult_n / (Juvenile_n + Adult_n)
-    Adult_pct = "{:.2%}".format(Adult_pct)
-    # Primary_Fur_Color
-    Black_n = sights.filter(Primary_Fur_Color='Black').count()
-    Gray_n = sights.filter(Primary_Fur_Color='Gray').count()
-    Cinnamon_n = sights.filter(Primary_Fur_Color='Cinnamon').count()
-    Black_pct = Black_n / (Black_n+Gray_n+Cinnamon_n)
-    Black_pct = "{:.2%}".format(Black_pct)
-    Gray_pct = Gray_n / (Black_n+Gray_n+Cinnamon_n)
-    Gray_pct = "{:.2%}".format(Gray_pct)
-    Cinnamon_pct = Cinnamon_n / (Black_n+Gray_n+Cinnamon_n)
-    Cinnamon_pct = "{:.2%}".format(Cinnamon_pct)
-    # Location
-    Above_Ground_n = sights.filter(Location='Above Ground').count()
-    Ground_Plane_n = sights.filter(Location='Ground Plane').count()
-    Above_Ground_pct = Above_Ground_n / (Above_Ground_n+Ground_Plane_n)
-    Above_Ground_pct = "{:.2%}".format(Above_Ground_pct)
-    Ground_Plane_pct = Ground_Plane_n / (Above_Ground_n+Ground_Plane_n)
-    Ground_Plane_pct= "{:.2%}".format(Ground_Plane_pct)
-    # Runs_From
-    True_n = sights.filter(Runs_From=True).count()
-    False_n = sights.filter(Runs_From=False).count()
-    True_pct = True_n / (True_n+False_n)
-    True_pct = "{:.2%}".format(True_pct)
-    False_pct = False_n / (True_n+False_n)
-    False_pct = "{:.2%}".format(False_pct)
-
-    context = {
-            'Total':sights.count(),
-            'Shift': {'AM': AM_n,'PM': PM_n},
-            'Shift_pct': {'AM': AM_pct,'PM': PM_pct},
-            'Age': {'Juvenile': Juvenile_n, 'Adult': Adult_n},
-            'Age_pct': {'Juvenile': Juvenile_pct, 'Adult': Adult_pct},
-            'Primary_Fur_Color': {'Black':Black_n, 'Gray':Gray_n, 'Cinnamon':Cinnamon_n},
-            'Primary_Fur_Color_pct': {'Black':Black_pct, 'Gray':Gray_pct, 'Cinnamon':Cinnamon_pct},
-            'Location': {'Above_Ground':Above_Ground_n, 'Ground_Plane':Ground_Plane_n},
-            'Location_pct': {'Above_Ground':Above_Ground_pct, 'Ground_Plane':Ground_Plane_pct},
-            'Runs_From': {'True':True_n, 'False':False_n},
-            'Runs_From_pct': {'True':True_pct, 'False':False_pct},
-            }
-    return render(request, 'Sightings/stats.html', {'context':context})
-
-
-
-
-# Create your views here.
+def stats(request):
+	squirrels = Sighting.objects.all()
+	total = len(squirrels)
+	lattitude = squirrels.aggregate(minimum=Min('Latitude'),maximum=Max('Latitude'))
+	longitude = squirrels.aggregate(minimum=Min('Longitude'),maximum=Max('Longitude'))
+	primary_fur_color =list(squirrels.values_list('Primary_Fur_Color').annotate(Count('Primary_Fur_Color')))
+	running = list(squirrels.values_list('Running').annotate(Count('Running')))
+	shift = list(squirrels.values_list('Shift').annotate(Count('Shift')))
+	context = {'total': total,
+		'lattitude': lattitude,
+		'longitude': longitude,
+		'primary_fur_color': primary_fur_color,
+		'running': running,
+		'shift': shift,
+		}
+	return render(request, 'sightings/stats.html', context)
